6e119eedfbe717f388c6c23a069e32921c463539
antontrmk <antontrmk@gmail.com>
2021-03-15 18:10:24 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let l = str.length;
+  let arr = [];
+  function checkIfOpening (bracket) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (bracket == bracketsConfig[i][0]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  function checkIfPair (lastArrayElement, bracket) 
+  {
+    for (let i = 0; i < bracketsConfig.length; i++) 
+    {
+      if ((bracketsConfig[i][0] == lastArrayElement) && (bracketsConfig[i][1] == bracket)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  for (let i = 0; i < l; i++) 
+  {
+    if (checkIfOpening(str[i]) && (checkIfPair(arr[arr.length - 1], str[i]) == false)) 
+    {
+      arr.push(str[i]);
+    } 
+    else 
+    {
+      if (checkIfPair(arr[arr.length - 1], str[i])) 
+      {
+        arr.pop();
+      }
+      else {
+        return false;
+      }
+    }
+  }
+  return arr.length == 0 ? true : false;
 }
